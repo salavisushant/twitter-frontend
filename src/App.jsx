2638aee84d3b2d041cb43b198d153eef2bda94df
Abhisingh755 <87421842+Abhisingh755@users.noreply.github.com>
2022-01-22 01:16:20 +0530

    Bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import { Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import { Home } from './components/Home';
 import {Profile } from './components/Profile'
+import {Bookmarks } from './components/Bookmarks'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path={"/register"} element={<Register />} />
         <Route path={"/home"} element={<Home />}/>
         <Route path={"/profile"} element={<Profile/>}/>
+        <Route path={"/bookmarks"} element={<Bookmarks/>}/>
         <Route path={"*"} element={<h1>Page not found</h1>}/>
 
 
---
+++ src/components/Bookmarks.jsx
@@ -0,0 +1,19 @@
+import "./Bookmarks.css"
+
+export const Bookmarks = () => {
+    return (
+        <>
+            <div className="bookmarks">
+                  <div className="arrow">         <div className="books">  <h2>Bookmarks</h2>
+                  <h3>@myusername</h3>
+                  </div> </div>
+
+                  <div className="tagline">
+                      <h1>You haven’t added any Tweets to your Bookmarks yet</h1>
+                      <p>When you do, they’ll show up here.</p>
+                  </div>
+
+                  </div>
+        </>
+    )
+}
